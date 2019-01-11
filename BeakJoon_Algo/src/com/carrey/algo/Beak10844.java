441a5fd48adb2601b20ccf7eed0f4fb5750f7969
jhm1643 <40678477+jhm1643@users.noreply.github.com>
2019-01-11 17:12:56 +0900

    11727, 9095, 10844, 11057 완료

--- BeakJoon_Algo/src/com/carrey/algo/Beak10844.java
+++ BeakJoon_Algo/src/com/carrey/algo/Beak10844.java
@@ -1,9 +1,30 @@
 package com.carrey.algo;
 
+import java.util.Scanner;
+
 public class Beak10844 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//LONG TYPE 주의 할것!!!
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		long dp[][] = new long[n+1][10];
+		for(int i=0;i<=9;i++) {
+			dp[1][i] = 1;
+		}
+		
+		for(int i=2; i<=n; i++) {
+			for(int j=0;j<10;j++) {
+				if(j==0) dp[i][j] = dp[i-1][1]%1000000000;
+				else if(j==9) dp[i][j] = dp[i-1][8]%1000000000;
+				else dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1])%1000000000;
+			}
+		}
+		long result=0;
+		for(int i=1;i<10;i++) {
+			result += dp[n][i];
+		}
+		System.out.println(result%1000000000);
 		
 	}
 
